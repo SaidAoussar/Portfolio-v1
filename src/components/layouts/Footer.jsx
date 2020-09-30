91f3a750637b42ac260828cfed750d54bf0850ec
SaidAoussar <aoussarsaid33@gmail.com>
2020-09-30 20:55:31 +0100

    create footer

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Box, Typography} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { SiWhitesource } from 'react-icons/si';
+
+
+const useStyles = makeStyles((theme)=>({
+  bgSection: {
+    backgroundColor :theme.palette.primary.main,
+    color: "white",
+  },
+}));
+
+function Footer() {
+  var classes = useStyles();
+  return (
+    <Box className={classes.bgSection} align="center" component="div" py={6} id="projects">
+      <Typography variant="h6">Designed & Built Said Aoussar</Typography>
+         
+    </Box>
+  )
+}
+
+export default Footer
